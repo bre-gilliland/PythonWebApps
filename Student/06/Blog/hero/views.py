b4915632cecf6b5be9598de126243d9ef88579b0
bre.gilliland <112044924+gillilandbre@users.noreply.github.com>
2023-11-01 10:47:57 -0600

    Beginning Project 6

---
+++ Student/06/Blog/hero/views.py
@@ -0,0 +1,73 @@
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'heroes.html'
+
+
+class HulkView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Hulk',
+            'body': 'My name is Bruce Banner',
+            'image': '/static/images/hulk.jpg'
+        }
+
+
+class IronManView(TemplateView):
+    template_name = "hero.html"
+
+    def get_context_data(self, **kwargs):
+        data = {
+            'title': 'Iron Man',
+            'body': 'My name is Tony Stark, but I am Iron Man',
+            'image': '/static/images/iron_man.jpg'
+        }
+        return data
+
+
+class BlackWidow(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Black Widow',
+            'body': 'My name is Natasha Romanova',
+            'image': '/static/images/black_widow.jpg'
+        }
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Student/03/Superhero
+
+
+class HeroListView(ListView):
+    template_name = 'hero/list.html'
+    model = Superhero
+
+
+class HeroDetailView(DetailView):
+    template_name = 'hero/detail.html'
+    model = Superhero
+
+
+class HeroCreateView(LoginRequiredMixin, CreateView):
+    template_name = "hero/add.html"
+    model = Superhero
+    fields = '__all__'
+
+
+class HeroUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "hero/edit.html"
+    model = Superhero
+    fields = '__all__'
+
+
+class HeroDeleteView(LoginRequiredMixin, DeleteView):
+    model = Superhero
+    template_name = 'hero/delete.html'
+    success_url = reverse_lazy('hero_list')

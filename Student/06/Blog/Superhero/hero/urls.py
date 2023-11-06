d20226d16de806aa7eead01d1af5fba08854a3f8
bre.gilliland <112044924+gillilandbre@users.noreply.github.com>
2023-11-06 15:43:06 -0700

    11/6-

--- Student/06/Blog/Superhero/hero/urls.py
+++ Student/06/Blog/Superhero/hero/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path
-from .views import BlackWidow, HulkView, IndexView, IronManView
+from django.urls import path, include
+from .views import HeroCreateView, HeroDeleteView, HeroDetailView, HeroListView, HeroUpdateView
+from django.contrib import admin
 
 urlpatterns = [
-    path('', IndexView.as_view()),
-    path('hulk', HulkView.as_view()),
-    path('ironman', IronManView.as_view()),
-    path('blackwidow', BlackWidow.as_view()),
+
+    # Hero
+    path('',                HeroListView.as_view(),    name='hero_list'),
+    path('<int:pk>',        HeroDetailView.as_view(),  name='hero_detail'),
+    path('add',             HeroCreateView.as_view(),  name='hero_add'),
+    path('<int:pk>/',       HeroUpdateView.as_view(),  name='hero_edit'),
+    path('<int:pk>/delete', HeroDeleteView.as_view(),  name='hero_delete'),
+
+    # Login/Logout code
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Admin views for users
+    path('admin/', admin.site.urls),
+    # path('admin/', include('admin.site.urls')),   Don't do this!
+
 ]
--- Student/06/Blog/Superhero/hero/views.py
+++ Student/06/Blog/Superhero/hero/views.py
@@ -1,39 +1,34 @@
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Superhero
 
 
-class IndexView(TemplateView):
-    template_name = 'heroes.html'
+class HeroListView(ListView):
+    template_name = 'hero/list.html'
+    model = Superhero
 
 
-class HulkView(TemplateView):
-    template_name = 'hero.html'
-
-    def get_context_data(self, **kwargs):
-        return {
-            'title': 'Hulk',
-            'body': 'My name is Bruce Banner',
-            'image': '/static/images/hulk.jpg'
-        }
+class HeroDetailView(DetailView):
+    template_name = 'hero/detail.html'
+    model = Superhero
 
 
-class IronManView(TemplateView):
-    template_name = "hero.html"
-
-    def get_context_data(self, **kwargs):
-        data = {
-            'title': 'Iron Man',
-            'body': 'My name is Tony Stark, but I am Iron Man',
-            'image': '/static/images/iron_man.jpg'
-        }
-        return data
+class HeroCreateView(LoginRequiredMixin, CreateView):
+    template_name = "hero/add.html"
+    model = Superhero
+    fields = '__all__'
 
 
-class BlackWidow(TemplateView):
-    template_name = 'hero.html'
+class HeroUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "hero/edit.html"
+    model = Superhero
+    fields = '__all__'
 
-    def get_context_data(self, **kwargs):
-        return {
-            'title': 'Black Widow',
-            'body': 'My name is Natasha Romanova',
-            'image': '/static/images/black_widow.jpg'
-        }
+
+class HeroDeleteView(LoginRequiredMixin, DeleteView):
+    model = Superhero
+    template_name = 'hero/delete.html'
+    success_url = reverse_lazy('hero_list')

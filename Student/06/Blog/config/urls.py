99478999e0c92b913f076fd60ed2f91fdc5467c8
bre.gilliland <112044924+gillilandbre@users.noreply.github.com>
2023-11-06 19:04:21 -0700

    116

--- Student/06/Blog/config/urls.py
+++ Student/06/Blog/config/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from hero.views import  HulkView, IronManView
 
 urlpatterns = [
-    path('',        HulkView.as_view()),
-     path('IronMan',       IronManView.as_view()),
+
+    path('hero/',                HeroListView.as_view(),    name='hero_list'),
+    path('hero/<int:pk>',        HeroDetailView.as_view(),  name='hero_detail'),
+    path('hero/add',             HeroCreateView.as_view(),  name='hero_add'),
+    path('hero/<int:pk>/',       HeroUpdateView.as_view(),  name='hero_edit'),
+    path('hero/<int:pk>/delete', HeroDeleteView.as_view(),  name='hero_delete'),
 
 ]
--- Student/06/Blog/hero/views.py
+++ Student/06/Blog/hero/views.py
@@ -1,4 +1,31 @@
-from django.views.generic import TemplateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+
+from .models import Superhero
+from .models import Superhero
+
+class HeroListView(ListView):
+    template_name = 'hero/list.html'
+    model = Superhero
+    context_object_name = 'heroes'
+
+class HeroDetailView(DetailView):
+    template_name = 'hero/detail.html'
+    model = Superhero
+    context_object_name = 'hero'
+
+class HeroCreateView(CreateView):
+    template_name = "hero/add.html"
+    model = Superhero
+    fields = '__all__'
+
+class HeroUpdateView(UpdateView):
+    template_name = "hero/edit.html"
+    model = Superhero
+    fields = '__all__'
+
+class HeroDeleteView(DeleteView):
+    model = Superhero
+    template_name = 'hero/delete.html'
 
 
 class IndexView(TemplateView):

6af528d9023654f9caedea6108f1b2ee812de141
bre.gilliland <112044924+gillilandbre@users.noreply.github.com>
2023-11-28 13:57:15 -0700

    tests.py changes

--- Student/TDD/config/Blog/management/commands/pages.py
+++ Student/TDD/config/Blog/management/commands/pages.py
@@ -3,4 +3,8 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print('my command script')
+       from requests import get
+
+response = get('https://google.com')
+print ('Google', response)
+print(response.text)
--- Student/TDD/config/Blog/tests.py
+++ Student/TDD/config/Blog/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase 
 
-# Create your tests here.
+from .models import Article
+
+
+class BlogAppTest(SimpleTestCase)
+    
+    def test_django(self):
+        self.assertTrue(True)
+
+class BlogDataTest(SimpleTestCase)
+    
+    def test_blog(self):
+        self.assertEqual(len(Blog.objects.all()), 0)
+        Article.objects.create(title='Title 1')
+        Article.objects.create(title='Title 2')
+        self.assertEqual(len(Blog.objects.all()), 1)
+
+    a = Article.objects.get(pk=2)
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title 
+    def get_absolute_url(self):
+        return reverse_lazy("article_list")
+    
